3dc52515d9661172868392eb82a4a621a74029e6
IlMon-dot <derevyashki43@gmail.com>
2026-09-12 07:31:00 +0300

    Названия и ссылки обновлены через редактор

--- products.js
+++ products.js
@@ -24,7 +24,7 @@
 const ТОВАРЫ = [
   {
     ключ: 'mashinka',
-    название: 'Багги на радиоуправлении',
+    название: 'Багги на радиоуправлении 111',
     герой: 'photo/mashinka-hero.webp',
     миниатюра: 'photo/mashinka-thumb.webp',
     сцена: 'photo/bg/mashinka.webp',
